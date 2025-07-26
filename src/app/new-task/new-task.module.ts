0705390b4eafad1e95d74261c2267a8f81f9fd2b
Iveth Rodriguez <ivethrodriguezmath@gmail.com>
2025-07-26 14:46:00 -0500

    feat: HTML with validations corresponding to adding and/or updating tasks is added.

--- src/app/new-task/new-task.module.ts
+++ src/app/new-task/new-task.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { NewTaskPageRoutingModule } from './new-task-routing.module';
-
 import { NewTaskPage } from './new-task.page';
+import { PriorityColorPipe } from '../pipes/priority-color.pipe';
 
 @NgModule({
-  imports: [CommonModule, IonicModule, NewTaskPageRoutingModule],
   declarations: [NewTaskPage],
+  imports: [
+    CommonModule,
+    IonicModule,
+    NewTaskPageRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    PriorityColorPipe,
+  ],
 })
 export class NewTaskPageModule {}
