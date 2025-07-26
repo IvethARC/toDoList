45d683554d4932c2cd5031e14e97a61a3fd1f712
Iveth Rodriguez <ivethrodriguezmath@gmail.com>
2025-07-26 10:14:25 -0500

    feat: Creation of app pages, general styles added

---
+++ src/app/new-task/new-task.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-task',
+  templateUrl: './new-task.page.html',
+  styleUrls: ['./new-task.page.scss'],
+  standalone: false,
+})
+export class NewTaskPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
